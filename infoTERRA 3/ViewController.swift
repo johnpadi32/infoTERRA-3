ba888879f5dfcde87c474263b0fc8f315aa29983
John Padilla <johnpadi8790@gmail.com>
2025-09-02 18:07:04 -0600

    Initial Commit

---
+++ infoTERRA 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  infoTERRA 3
+//
+//  Created by John Padilla on 9/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

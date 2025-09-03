6f8e74a92658e2db59ad13296bafaea64e04ba10
John Padilla <johnpadi8790@gmail.com>
2025-09-02 23:41:40 -0600

    TabBarController

--- infoTERRA 3/BaseTabBarControllers.swift
+++ infoTERRA 3/BaseTabBarControllers.swift
@@ -5,4 +5,34 @@
 //  Created by John Padilla on 9/2/25.
 //
 
-import Foundation
+import UIKit
+
+class BaseTabBarControllers: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "infoTERRA", imageName: "Home", image: "house.fill"),
+            createNavController(viewController: UIViewController(), title: "Search", imageName: "Search", image: "magnifyingglass"),
+            createNavController(viewController: UIViewController(), title: "Categories", imageName: "Categories", image: "lineweight")
+
+
+        ]
+    }
+    
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String, image: String) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .white
+        
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(systemName: image)
+        
+        return navController
+        
+    }
+}
